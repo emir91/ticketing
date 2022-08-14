e66c64027eb288447fd67940adfd26c6ed7d4e37
Emir Vranac <vranacemir@gmail.com>
2022-08-14 15:28:05 +0200

    Adding validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  res.send("Sign Up route");
-});
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Passowrd must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    res.send("Sign Up route");
+  }
+);
 
 export default router;
