25cf33cfe74d420ccea06b8b33fd6c1de2e621e3
Emir Vranac <vranacemir@gmail.com>
2022-08-14 15:46:12 +0200

    Handling validation errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
@@ -13,9 +13,16 @@ router.post(
       .withMessage("Passowrd must be between 4 and 20 characters"),
   ],
   (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
     const { email, password } = req.body;
 
-    res.send("Sign Up route");
+    console.log("Creating a user...");
+
+    res.send({});
   }
 );
 
