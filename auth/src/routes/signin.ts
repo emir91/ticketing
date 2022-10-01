22ad24f9947538a33296132360ad350ef893ff47
Emir Vranac <vranacemir@gmail.com>
2022-10-01 14:19:24 +0200

    Signin flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Sign In route");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Provide valid email"),
+    body("password").trim().isEmpty().withMessage("Provide valid password"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export default router;
