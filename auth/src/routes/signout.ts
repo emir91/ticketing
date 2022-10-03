880d3326daa260f25b1eb220fdc4d3edf2d85222
Emir Vranac <vranacemir@gmail.com>
2022-10-03 14:37:33 +0200

    Signing out

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Sign Out route");
+  req.session = null;
+
+  res.send({});
 });
 
 export default router;
