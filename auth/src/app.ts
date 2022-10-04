e59240e7323cea60f79fa6e8be44719a5d26137a
Emir Vranac <vranacemir@gmail.com>
2022-10-04 13:26:33 +0200

    Index to App refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import currentUserRouter from "./routes/current-user";
+import signInRouter from "./routes/signin";
+import signOutRouter from "./routes/signout";
+import signUpRouter from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors";
+
+const app = express();
+
+app.use(json());
+app.set("trust proxy", true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
