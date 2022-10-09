fda03a78ddfc01b499ebe6e910c2e172cfe0513b
Emir Vranac <vranacemir@gmail.com>
2022-10-09 11:40:51 +0200

    Handling form inputs

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,27 @@
+import { useState } from "react"
+
 const SignupForm = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitHandler = e => {
+        e.preventDefault()
+
+        console.log('form submitted!', {
+           email, password 
+        });
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <h1>Sign Up</h1>
             <div className="form-group">
                 <label>Email</label>
-                <input className="form-control"/>
+                <input className="form-control" onChange={e => setEmail(e.target.value)}/>
             </div>
             <div className="form-group">
                 <label>Password</label>
-                <input type="password" className="form-control"/>
+                <input type="password" className="form-control" onChange={e => setPassword(e.target.value)}/>
             </div>
             <button className="btn btn-primary">Sign Up</button>
         </form>
