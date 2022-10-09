b6a4895a2adfe151c17528558a1e797ae912b4d7
Emir Vranac <vranacemir@gmail.com>
2022-10-09 14:38:02 +0200

    Install axios and handle displaying form validation errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,26 @@
 import { useState } from "react"
+import axios from "axios"
 
 const SignupForm = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState([])
 
-    const submitHandler = e => {
+    const submitHandler = async (e) => {
         e.preventDefault()
+        try {
+            if(errors.length >= 1) {
+                setErrors([])
+            }
+            const response = await axios.post("/api/users/signup", { email, password} )
+            console.log('form submitted!', response.data);
 
-        console.log('form submitted!', {
-           email, password 
-        });
+        } catch (error) {
+            setErrors(error.response.data.errors)
+            console.log('Error', error.response.data.errors);        
+        }
     }
+    
 
     return (
         <form onSubmit={submitHandler}>
@@ -18,10 +28,19 @@ const SignupForm = () => {
             <div className="form-group">
                 <label>Email</label>
                 <input className="form-control" onChange={e => setEmail(e.target.value)}/>
+                <p className="text-danger">
+                    {(errors.some(e => e.field === 'email') || !errors[0]?.hasOwnProperty('field')) && errors[0]?.message}
+                </p>
             </div>
             <div className="form-group">
                 <label>Password</label>
                 <input type="password" className="form-control" onChange={e => setPassword(e.target.value)}/>
+                <p className="text-danger">
+                    {
+                     ((errors.some(e => e.field === 'password') && errors.length > 1) && errors[1].message) || 
+                     (((errors.some(e => e.field === 'password') && errors.length === 1) && errors[0].message))
+                    }
+                </p>
             </div>
             <button className="btn btn-primary">Sign Up</button>
         </form>
