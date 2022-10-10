7d7251fdb3355c65f9432259204a534f4fbf3902
Emir Vranac <vranacemir@gmail.com>
2022-10-10 11:17:15 +0200

    Add onSuccess callback to route user after successfull signup

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import axios from "axios";
 
-const useRequest = ({url, method, body}) => {
+const useRequest = ({url, method, body, onSuccess}) => {
     const [errors, setErrors] = useState(null)
 
     const doRequest = async () => {
         try {
             setErrors([])
             const response = await axios[method](url, body)
+            if(onSuccess) {
+                onSuccess(response.data)
+            }
             return response.data
 
         } catch (error) {
-            setErrors(error.response.data.errors)
+            setErrors(error.response?.data.errors)
         }
     }
     return {doRequest, errors}
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Router from "next/router"
 import useRequest from "../../hooks/use-request"
 
 const SignupForm = () => {
@@ -8,12 +9,13 @@ const SignupForm = () => {
     const {doRequest, errors} = useRequest({
         url: "/api/users/signup", 
         method: 'post', 
-        body: {email, password}
+        body: {email, password},
+        onSuccess: () => Router.push('/')
     })
 
-    const submitHandler =  (e) => {
+    const submitHandler =  async (e) => {
         e.preventDefault()
-        doRequest()
+        await doRequest()
     }
     
     return (
