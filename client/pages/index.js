9663d92d23d2515c5529d50cbe31891d58be4206
Emir Vranac <vranacemir@gmail.com>
2022-10-11 15:04:50 +0200

    Handle SSR data fetching

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,16 @@
+import buildCient from "../api/build-client"
+
 const Home = () => {
     return (
         <h1>Home Page</h1>
     )
+}
+
+Home.getInitialProps = async (context) => {
+    const client = buildCient(context)
+    const { data } = await client .get('/api/users/currentuser')
+
+   return data
 }
 
 export default Home 
