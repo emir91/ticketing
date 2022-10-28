00e994424f582ba88dac2231da7096044f241b87
Emir Vranac <vranacemir@gmail.com>
2022-10-28 14:21:30 +0200

    Add some request validation logic to the new router handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,23 @@
 import { Router, Request, Response } from "express";
+import { requireAuth, validateRequest } from "@emir-tickets/common";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .isMongoId()
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export default router;
