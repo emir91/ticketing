84389f70367a8acd98b609d979735030f471732f
Emir Vranac <vranacemir@gmail.com>
2022-11-01 11:14:16 +0100

    Defining Ticket model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,45 @@
+import { Schema, model } from "mongoose";
+import { OrderStatus } from "@emir-tickets/common";
+
+interface OrderModel {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+const orderSchema = new Schema<OrderModel>(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+    },
+
+    expiresAt: {
+      type: Schema.Types.Date,
+    },
+
+    ticket: {
+      type: Schema.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+const Order = model<OrderModel>("Order", orderSchema);
+
+export { Order };
---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+interface TicketModel {
+  title: string;
+  price: number;
+}
+
+const ticketSchema = new Schema<TicketModel>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+const Ticket = model<TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
