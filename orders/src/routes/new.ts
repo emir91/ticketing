f34a59ce1477386e88dcba4bae7f50cb44fde870
Emir Vranac <vranacemir@gmail.com>
2022-11-01 12:28:22 +0100

    Finding reserved ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,14 @@
 import { Router, Request, Response } from "express";
-import { requireAuth, validateRequest } from "@emir-tickets/common";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@emir-tickets/common";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = Router();
 
@@ -16,6 +24,30 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete,
+          OrderStatus.Created,
+        ],
+      },
+    });
+
+    if (existingOrder) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
     res.send({});
   }
 );
